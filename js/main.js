bc9b22e2db9b4e2578237d2e3c875b285c25a201
Christine Echo <christineecho@outlook.com>
2020-08-22 20:49:45 -0600

    checking a checkbox now moves to-do over to the completed column;

--- js/main.js
+++ js/main.js
@@ -19,10 +19,15 @@ function listNewToDo() {
     document.getElementById("active-list").innerHTML += `<li id="li-${newToDo.id}"><input type="checkbox" id="done-${newToDo.id}" name="done-${newToDo.id}" value="done-${newToDo.id}" onclick="completeToDo(${newToDo.id})"><label for="done-${newToDo.id}">${toDoList[newToDo.id].task}<strong> Start: </strong>${toDoList[newToDo.id].startDate}</label><button id="complete-${newToDo.id}" class="button">Delete</button></li>`;
     newToDo = "";
 } 
+function listNewCompletedToDo(completedId) {
+    document.getElementById("completed-list").innerHTML += `<li>${completedList[completedId].task}<strong> Start: </strong>${completedList[completedId].startDate}</label><button id="complete-${completedList.completedId}" class="button">Delete</button></li>`;
+}
 
 function completeToDo(id) {
     completedList.push(toDoList[id]);
+    let completedId = completedList.length-1;
     completedList.endDate = new Date("2015-03-25t12:00:00Z");
+    listNewCompletedToDo(completedId);
     let noLongerActive = document.querySelector(`#li-${id}`);
     noLongerActive.remove();
     toDoList.splice(id, 1); // Removes array element at index position
