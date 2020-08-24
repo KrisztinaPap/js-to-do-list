b80168627852fe947ea0a1520f81b541470ad568
Christine Echo <christineecho@outlook.com>
2020-08-24 11:23:23 -0600

    Rolled things back; Restructured JS; Adds user input to pending array feature;

--- js/main.js
+++ js/main.js
@@ -3,40 +3,45 @@ let newToDo = {};
 let pendingList = []; // Initializes an empty array
 let activeList = []; // Initializes an empty array
 let completedList = []; // Initializes an empty array
-let userInput = document.getElementById("newtask").value;
-let htmlPendingList = document.getElementById("pending-list").innerHTML;
-let htmlActiveList = document.getElementById("active-list").innerHTML;
-let htmlCompletedList = document.getElementById("completed-list").innerHTML;
+let userInput = document.getElementById( "newtask" );
+let htmlPendingList = document.getElementById( 'pending-list' ).innerHTML;
+let htmlActiveList = document.getElementById( 'active-list' ).innerHTML;
+let htmlCompletedList = document.getElementById( 'completed-list' ).innerHTML;
+let deleteButton = document.querySelectorAll( '.deleteButton ').innerHTML;
 
+/* function deleteButtonClick(id) {
+    deleteButton.addEventListener( ' click ', ( id ) => {
+        console.log( `A Delete button was pressed! ${id}` );
+    });
+} */
 
-function addNewToDo() {
+function createNewToDo( userInput ) {
     newToDo = {
         id: Math.random(),
-        task: userInput.trim(), // Takes input and trims off leading/trailing white spaces
-        addedDate: new Date("2015-03-25t12:00:00Z"), // Citation: https://www.w3schools.com/js/js_date_formats.asp; date formatting
+        task: userInput.value, // Takes input and trims off leading/trailing white spaces
+        addedDate: new Date( "2015-03-25t12:00:00Z" ), // Citation: https://www.w3schools.com/js/js_date_formats.asp; date formatting
         startDate: "",
         endDate: ""
     };
-    pendingList.push(newToDo);
-    console.log("Pending:", pendingList);
-    displayPendingToDo(newToDo.id);
+    addPendingToDo( newToDo );
+    reloadPendingList();
 }
 
-function displayPendingToDo(id) {
-    let index = pendingList.indexOf(newToDo);
-    let listItem = 
-        `<li id="li-${id}">
-            ${pendingList[index].task}
-            <button id="start-${id} onclick="activateToDo(${id}, e), e.preventDefault()">Start</button>
-            <button id="delete-${id}">Delete</button>
-        </li>`;
-    let position = "beforeend";
-    htmlPendingList.insertAdjacentHTML( position, listItem);
+function addPendingToDo( newToDo ) {
+    pendingList.push( newToDo );
+    console.log( "Pending:", pendingList );
 }
 
-function activateToDo(index) {
+/* function activateToDo(index) {
     pendingList[index].startDate = new Date("2015-03-25t12:00:00Z"); // Citation: https://www.w3schools.com/js/js_date_formats.asp; date formatting
     activeList.push(pendingList[index]);
     console.log("Pending:", pendingList);
     console.log("Active", activeList);
+} */
+
+function reloadPendingList() {
+    htmlPendingList = "";
+    for ( let i = 0; i < pendingList.length; i++ ) {
+        htmlPendingList += pendingList[i].task;
+    }
 }
